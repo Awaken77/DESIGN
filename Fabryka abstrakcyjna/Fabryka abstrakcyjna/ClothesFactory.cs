c1871842bc64c18b5121d6dbd35a106bc61a76fe
Awaken77 <73673353+Awaken77@users.noreply.github.com>
2022-07-02 19:18:18 +0200

    Add files via upload

---
+++ Fabryka abstrakcyjna/Fabryka abstrakcyjna/ClothesFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabryka_abstrakcyjna
+{
+    abstract class ClothesFactory
+    {
+        public abstract Shirt CreateShirt();
+        public abstract Trousers CreateTrousers();
+    }
+}
---
+++ Fabryka abstrakcyjna/Fabryka abstrakcyjna/Factories/CasualClothesFactory.cs
@@ -0,0 +1,22 @@
+﻿using Fabryka_abstrakcyjna.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabryka_abstrakcyjna
+{
+    class CasualClothesFactory : ClothesFactory
+    {
+        public override Shirt CreateShirt()
+        {
+            return new PoloShirt();
+        }
+
+        public override Trousers CreateTrousers()
+        {
+            return new Jeans();
+        }
+    }
+}
