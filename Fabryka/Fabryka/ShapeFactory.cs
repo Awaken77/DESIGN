5a797ef2c0ca5d149fade956801c41265f59274a
Awaken77 <73673353+Awaken77@users.noreply.github.com>
2022-07-02 19:16:03 +0200

    Add files via upload

---
+++ Fabryka/Fabryka/ShapeFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabryka
+{
+    public class ShapeFactory
+    {
+        public Shape CreateShape(ShapeType type)
+        {
+            switch (type)
+            {
+                case ShapeType.Circle:
+                    return new Circle();
+                case ShapeType.Triangle:
+                    return new Triangle();
+                case ShapeType.Rectangle:
+                    return new Rectangle();
+                default:
+                    throw new Exception($"Shape type {type} is not handled");
+
+            }
+        }
+
+    }
+}
