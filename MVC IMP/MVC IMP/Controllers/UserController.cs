07a719af6ee19c536f023b32f2e8030bae0edd36
Awaken77 <73673353+Awaken77@users.noreply.github.com>
2022-07-04 01:40:00 +0200

    Add files via upload

---
+++ MVC IMP/MVC IMP/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_IMP.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new Models.UserModel
+            {
+                Name = "Andrzej",
+                Surname = "Fogiel",
+                Address = "POW 3/5, 90-100 Łódź"
+            };
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            var model = new Models.UserModel
+            {
+                Name = "Andrzej",
+                Surname = "Fogiel",
+                Address = "POW 3/5, 90-100 Łódź"
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Models.UserModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ MVC IMP/MVC IMP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_IMP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
