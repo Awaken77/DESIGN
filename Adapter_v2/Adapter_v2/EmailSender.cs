7074ee055bf508272e87b1894244df3ff5f68e0f
Awaken77 <73673353+Awaken77@users.noreply.github.com>
2022-07-03 03:36:08 +0200

    Add files via upload

---
+++ Adapter_v2/Adapter_v2/EmailSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_v2
+{
+    public class EmailSender : INotificationSender
+    {
+        public void SendNotification(int userId, Notification notification)
+        {
+            Console.WriteLine($"Sending e-mail notification, to:{userId}, Title:{notification.Title}, Body:{notification.Body}");
+        }
+    }
+}
---
+++ Adapter_v2/Adapter_v2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            INotificationSender notificationSender = new SmsSenderAdapter();
+            notificationSender.SendNotification(1, new Notification() { Title = "Test", Body = "Body" });
+        }
+    }
+}
